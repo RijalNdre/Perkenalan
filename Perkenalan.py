5f93b985d28516ecab631c9b49bace316df5a5da
RijalNdre <93868952+RijalNdre@users.noreply.github.com>
2021-11-07 20:45:35 +0700

    Add files via upload

---
+++ Perkenalan.py
@@ -0,0 +1,15 @@
+nama = 'Rizal Andrias Mahardika'
+nim = '212410102047'
+kelompok = 'Spectre'
+umur = 19
+tinggi_badan = 166
+berat_badan = 60
+jomblo = True
+
+print('Nama                 :', nama)
+print('NIM                  :', nim)
+print('Kelompok             :', kelompok)
+print('Umur                 :', umur, 'tahun')
+print('Tinggi Badan         :', tinggi_badan, 'cm')
+print('Berat Badan          :', berat_badan, 'kg')
+print('Jomblo               :', jomblo)
